44dd905f4b07c596b83d939a0ff5c98b3efbfdff
mjesusoj <mjesusoj@proton.me>
2023-02-09 12:07:30 +0100

    UseEffect apply to counter and continue :rocket:

--- 01-bases/src/App.tsx
+++ 01-bases/src/App.tsx
@@ -1,11 +1,13 @@
 import { Counter } from "./bases/Counter"
 import { CounterBy } from "./bases/CounterBy"
+import { CounterEffect } from "./bases/CounterEffect"
 
 function App() {
   return (
       <>
           <Counter initialValue={ 15 } />
           <CounterBy />
+          <CounterEffect />
       </>
   )
 }
