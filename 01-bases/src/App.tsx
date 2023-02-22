18788b9607e5b6014890f893038dbf2939fb0b35
mjesusoj <mjesusoj@proton.me>
2023-02-22 13:27:25 +0100

    Separate logic from Counter Reducer & finish first section :sparkles:

--- 01-bases/src/App.tsx
+++ 01-bases/src/App.tsx
@@ -2,7 +2,8 @@ import { Counter } from "./bases/Counter"
 import { CounterBy } from "./bases/CounterBy"
 import { CounterEffect } from "./bases/CounterEffect"
 import { CounterHook } from "./bases/CounterHook"
-import { CounterReducerComponent } from "./bases/CounterReducer"
+import { CounterReducerComponent } from "./counter-reducer/CounterReducer"
+/* import { CounterReducerComponent } from "./bases/CounterReducer" */
 
 function App() {
   return (
---
+++ 01-bases/src/counter-reducer/interfaces/interfaces.ts
@@ -0,0 +1,5 @@
+export interface CounterState {
+    counter : number;
+    previous: number;
+    changes : number;
+}
