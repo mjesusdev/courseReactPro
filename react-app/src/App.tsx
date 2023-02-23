f4d90ec00da2518b9011c5f499012e824cba2aef
mjesusoj <mjesusoj@proton.me>
2023-02-23 13:32:01 +0100

    Create navigation of the page with react-routerv6 :sparkles:

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,9 @@
+import { Navigation } from './routes/Navigation';
+
 function App() {
   return (
     <>
-      <h1>App.tsx</h1>
+      <Navigation />
     </>
   )
 }
---
+++ react-app/src/routes/Navigation.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom'
+
+import logo from '../assets/logo.svg'
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+        <div className='main-layout'>
+            <nav>
+                <img src={ logo } alt="React logo" />
+                <ul>
+                  <li>
+                      <NavLink to='/home' className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                  </li>
+                  <li>
+                      <NavLink to='/about' className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
+                  </li>
+                  <li>
+                      <NavLink to='/users' className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
+                  </li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path='about' element={ <h1>About Page</h1> } />
+                <Route path='users' element={ <h1>Users Page</h1> } />
+                <Route path='home' element={ <h1>Home Page</h1> } />
+                
+                <Route path='/*' element={ <Navigate to='/home' replace /> } />
+            </Routes>
+        </div>
+    </BrowserRouter>
+  )
+}
