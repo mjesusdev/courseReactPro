20060612efd6f94afba7c93c5a1a461f4a55de28
mjesusoj <mjesusoj@proton.me>
2023-03-06 13:16:07 +0100

    Separate logic from ProductCard to new hook :rocket:

--- 02-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 02-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -1,14 +1,11 @@
+import { useProduct } from '../hooks/useProduct'
+
 import styles from '../styles/styles.module.css'
 import noImage from '../assets/no-image.jpg'
-import { useState } from 'react'
 
 export const ProductCard = () => {
 
-    const [ counter, setCounter ] = useState(0)
-
-    const increaseBy = ( value:number ) => {
-        setCounter( prev => Math.max( prev + value, 0 ))
-    }
+    const { counter, increaseBy } = useProduct()
 
     return (
         <div className={ styles.productCard }>
