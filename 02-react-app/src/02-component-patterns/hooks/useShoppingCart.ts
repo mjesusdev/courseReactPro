50aee7eded127464dbb03d91d5300d72cc4cce12
mjesusoj <mjesusoj@proton.me>
2023-05-05 13:51:29 +0200

    Upload new branch to make changes, control props alt :rocket:

--- 02-react-app/src/02-component-patterns/hooks/useShoppingCart.ts
+++ 02-react-app/src/02-component-patterns/hooks/useShoppingCart.ts
@@ -5,23 +5,19 @@ import { products } from '../data/products';
 
 export const useShoppingCart = () => {
     const [ shoppingCart, setShoppingCart ] = useState<{ [key:string]: ProductInCart }>({});
-
-    const [ product1, product2 ] = products;
-
+    
     const onProductCountChange = ({ count, product }: { count: number, product: Product } ) => {
         setShoppingCart( oldShoppingCart => {
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-            if ( Math.max( productInCart.count + count, 0 ) > 0 ) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
+            
+            if ( count === 0 ) {
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                return rest;
             }
             
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart
-            return rest;
+            return {
+                ...oldShoppingCart,
+                [ product.id ]: { ...product, count }
+            }
         })
     }
 
