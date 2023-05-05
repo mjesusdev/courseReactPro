3e1a6c3bf368917cac231eea4829b561ea8213c6
mjesusoj <mjesusoj@proton.me>
2023-05-05 13:45:01 +0200

    Finish shopping cart, control props finished :rocket:

--- 02-react-app/src/02-component-patterns/hooks/useProduct.ts
+++ 02-react-app/src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 
 interface useProductArgs {
@@ -10,7 +10,13 @@ interface useProductArgs {
 export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
     const [ counter, setCounter ] = useState( value );
     
+    const isControlled = useRef( !!onChange )
+
     const increaseBy = ( value: number ) => {
+        if ( isControlled.current ) {
+            return onChange!({ count: value, product }) 
+        }
+
         const newValue = Math.max( counter + value, 0 )
         setCounter( newValue );
         
--- 02-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 02-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,43 +1,10 @@
-import { useState } from 'react';
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
 
-import { Product } from '../interfaces/interfaces';
+import { useShoppingCart } from '../hooks/useShoppingCart';
 import '../styles/custom-styles.css';
 
-const product1 = {
-    id: '1',
-    title: 'Coffee Mug - Card',
-    img: './coffee-mug.png'
-}
-
-const product2 = {
-    id: '2',
-    title: 'Coffee Mug - Meme',
-    img: './coffee-mug2.png'
-}
-
-const products: Product[] = [ product1, product2 ]
-
-interface ProductInCard extends Product {
-    count: number
-}
-
 export const ShoppingPage = () => {
-    const [ shoppingCart, setShoppingCart ] = useState<{ [key:string]: ProductInCard }>({});
-
-    const onProductCountChange = ({ count, product }: { count: number, product: Product } ) => {
-        setShoppingCart( oldShoppingCard => {
-            if ( count === 0 ) {
-                const { [product.id]: toDelete, ...rest } = oldShoppingCard
-                return rest
-            }
-
-            return {
-                ...oldShoppingCard,
-                [ product.id ]: { ...product, count }
-            }
-        })
-    }
+    const { products, shoppingCart, onProductCountChange } = useShoppingCart();
 
     return (
         <div>
